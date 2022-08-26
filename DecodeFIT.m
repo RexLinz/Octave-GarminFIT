5c76a32a92aff9123fb76b7a2b95d2e6cad6048a
Roland Exler <roland.exler@a1.net>
2022-08-26 21:15:09 +0200

    completed conversion to array of struct

--- DecodeFIT.m
+++ DecodeFIT.m
@@ -10,99 +10,100 @@ if id<0 error("could not open input file"); end
 FITheader = readFITheader(id);
 messages = {};
 data = NaN*zeros(256,1);
-%while !feof(id)
-for n=1:2
+while !feof(id)
+%for n=1:35
   clear record;
   record.header = fread(id, 1);
+  if feof(id) break; end;
   if bitand(record.header, 128)
-    record = readFITtimestamp(id, record); % TODO (untested)
     disp("compressed timestamp");
+    record = readFITtimestamp(id, record); % TODO (untested)
   elseif bitand(record.header, 64)
     msgID = bitand(record.header, 15);
+    disp(["definition " num2str(msgID)]);
     record = readFITdefinition(id, record);
     if !feof(id) % eof will occour while reading CRC instead
       messages{msgID+1} = record;
-      disp(["definition " num2str(msgID)]);
     end
   else
     msgID = bitand(record.header, 15);
     record = messages{msgID+1};
-    if 1
+    if 0
       for f=1:record.fields
         [dummy,recSize] = fread(id, record.field(f).fieldSize); % skip record content
       end
     else
-      totalDone = 0;
+      totalSize = 0;
       filePos = ftell(id);
       for f = 1:record.fields
-        switch record.baseType(f)
+        totalSize += record.field(f).fieldSize;
+        switch record.field(f).baseType
           case 0
-            data(record.fieldNum(f)+1) = fread(id, 1, "uint8"); % enum
+            temp = fread(id, 1, "uint8"); % enum
             dataDone = 1;
           case 1
-            data(record.fieldNum(f)+1) = fread(id, 1, "int8"); % int8
+            temp = fread(id, 1, "int8"); % int8
             dataDone = 1;
           case 2
-            data(record.fieldNum(f)+1) = fread(id, 1, "uint8"); % uint8
+            temp = fread(id, 1, "uint8"); % uint8
             dataDone = 1;
           case 131 % 0x83
-            data(record.fieldNum(f)+1) = fread(id, 1, "int16"); % int16
+            temp = fread(id, 1, "int16"); % int16
             dataDone = 2;
           case 132 % 0x84
-            data(record.fieldNum(f)+1) = fread(id, 1, "uint16"); % uint16
+            temp = fread(id, 1, "uint16"); % uint16
             dataDone = 2;
           case 133 % 0x85
-            data(record.fieldNum(f)+1) = fread(id, 1, "int32"); % int32
+            temp = fread(id, 1, "int32"); % int32
             dataDone = 4;
           case 134 % 0x86
-            data(record.fieldNum(f)+1) = fread(id, 1, "uint32"); % uint32
+            temp = fread(id, 1, "uint32"); % uint32
             dataDone = 4;
           case 7
-            [dummy,dataDone] = fread(id, record.fieldSize(f), "uchar"); % TODO string
-            dataDone = record.fieldSize(f);
+            temp = fread(id, record.field(f).fieldSize, "uchar"); % TODO string
+            dataDone = record.field(f).fieldSize;
           case 136 % 0x88
-            data(record.fieldNum(f)+1) = fread(id, 1, "float"); % float
+            temp = fread(id, 1, "float"); % float
             dataDone = 4;
           case 137 % 0x89
-            data(record.fieldNum(f)+1) = fread(id, 1, "double"); % double
+            temp = fread(id, 1, "double"); % double
             dataDone = 8;
           case 10
-            data(record.fieldNum(f)+1) = fread(id, 1, "uint8"); % uint8z
+            temp = fread(id, 1, "uint8"); % uint8z
             dataDone = 1;
           case 139 % 0x8B
-            data(record.fieldNum(f)+1) = fread(id, 1, "uint16"); % uint16
+            temp = fread(id, 1, "uint16"); % uint16
             dataDone = 2;
           case 140 % 0x8C
-            data(record.fieldNum(f)+1) = fread(id, 1, "uint32"); % uint32
+            temp = fread(id, 1, "uint32"); % uint32
             dataDone = 4;
           case 13
-            [dummy,dataDone] = fread(id, record.fieldSize(f), "uint8"); % array of byte
+            temp = fread(id, record.field(f).fieldSize, "uint8"); % array of byte
             dataDone = record.fieldSize(f);
           case 142 % 0x8E
-            data(record.fieldNum(f)+1) = fread(id, 1, "int64"); % int64
+            temp = fread(id, 1, "int64"); % int64
             dataDone = 8;
           case 134 % 0x8F
-            data(record.fieldNum(f)+1) = fread(id, 1, "uint64"); % uint64
+            temp = fread(id, 1, "uint64"); % uint64
             dataDone = 8;
           case 134 % 0x90
-            data(record.fieldNum(f)+1) = fread(id, 1, "uint64"); % uint64z
+            temp = fread(id, 1, "uint64"); % uint64z
             dataDone = 8;
           otherwise
-            [dummy,dataDone] = fread(id, record.fieldSize(f), "uint8"); % unknown type
+            temp = fread(id, record.field(f).fieldSize, "uint8"); % unknown type
             warning("unknown base type found");
         end
-        if dataDone != record.fieldSize(f)
+        if dataDone != record.field(f).fieldSize
           warning("data read has different size than expected");
         end
-        totalDone += dataDone;
+        if length(temp)==1
+          data(record.field(f).fieldNum+1) = temp;
+        end
       end
-      if totalDone != sum(record.fieldSize)
-        warning("total record read does not match sum of field sizes");
-      end
-      fseek(id, filePos+sum(record.fieldSize)); % ensure to go on correct
+      fseek(id, filePos+totalSize); % ensure to go on correct
     end
 
-%    [dummy,devSize] = fread(id, sum(record.devSize)); % skip developer content
+    % skip developer content if available
     if record.devFields>0
       for f=1:record.devFields
         [dummy,recSize] = fread(id, record.devField(f).fieldSize); % skip record content
