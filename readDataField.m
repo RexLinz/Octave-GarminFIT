94b5d2f7a765f1364224acb38a6626fe80c92c63
Roland Exler <roland.exler@a1.net>
2022-08-28 11:02:24 +0200

    disabled check for "invalid" numbers (slow)

--- readDataField.m
+++ readDataField.m
@@ -5,31 +5,31 @@ filePos = ftell(id);
 switch field.baseType % binary representation
   case 0
     data = fread(id, 1, "uint8"); % enum
-    if data==255, data=NA; end
+%    if data==255, data=NA; end
     bytesDone = 1;
   case 1
     data = fread(id, 1, "int8"); % int8
-    if data==127, data=NA; end
+%    if data==127, data=NA; end
     bytesDone = 1;
   case 2
     data = fread(id, 1, "uint8"); % uint8
-    if data==255, data=NA; end
+%    if data==255, data=NA; end
     bytesDone = 1;
   case 131 % 0x83
     data = fread(id, 1, "int16"); % int16
-    if data==32767, data=NA; end
+%    if data==32767, data=NA; end
     bytesDone = 2;
   case 132 % 0x84
     data = fread(id, 1, "uint16"); % uint16
-    if data==65536, data=NA; end
+%    if data==65536, data=NA; end
     bytesDone = 2;
   case 133 % 0x85
     data = fread(id, 1, "int32"); % int32
-    if data==hex2dec("7FFFFFFF"), data=NA; end
+%    if data==hex2dec("7FFFFFFF"), data=NA; end
     bytesDone = 4;
   case 134 % 0x86
     data = fread(id, 1, "uint32"); % uint32
-    if data==hex2dec("FFFFFFFF"), data=NA; end
+%    if data==hex2dec("FFFFFFFF"), data=NA; end
     bytesDone = 4;
   case 7
     % TODO null terminated string untested
@@ -45,15 +45,15 @@ switch field.baseType % binary representation
     bytesDone = 8;
   case 10
     data = fread(id, 1, "uint8"); % uint8z
-    if data==0, data=NA; end
+%    if data==0, data=NA; end
     bytesDone = 1;
   case 139 % 0x8B
     data = fread(id, 1, "uint16"); % uint16z
-    if data==0, data=NA; end
+%    if data==0, data=NA; end
     bytesDone = 2;
   case 140 % 0x8C
     data = fread(id, 1, "uint32"); % uint32z
-    if data==0, data=NA; end
+%    if data==0, data=NA; end
     bytesDone = 4;
   case 13
     data = fread(id, record.field(f).fieldSize, "uint8"); % array of byte
@@ -61,15 +61,15 @@ switch field.baseType % binary representation
     bytesDone = record.fieldSize(f);
   case 142 % 0x8E
     data = fread(id, 1, "int64"); % int64
-    if data==hex2dec("7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"), data=NA; end
+%    if data==hex2dec("7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"), data=NA; end
     bytesDone = 8;
   case 134 % 0x8F
     data = fread(id, 1, "uint64"); % uint64
-    if data==hex2dec("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"), data=NA; end
+%    if data==hex2dec("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"), data=NA; end
     bytesDone = 8;
   case 134 % 0x90
     data = fread(id, 1, "uint64"); % uint64z
-    if data==0, data=NA; end
+%    if data==0, data=NA; end
     bytesDone = 8;
   otherwise
     data = fread(id, record.field(f).fieldSize, "uint8"); % unknown type
