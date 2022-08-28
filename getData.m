7dbb736e7a5b482b56e820c8bfcff37a5b60e21a
Roland Exler <roland.exler@a1.net>
2022-08-28 12:10:48 +0200

    added help

--- getData.m
+++ getData.m
@@ -1,5 +1,14 @@
 function data = getData(record, name)
 
+% function data = getData(record, name)
+% read named data from activity records (messageNumber=20)
+% accepted NAME values are lat, lon, alt, heart, cad, dist, speed,time
+% output is converted to useful units, e.g. deg, m, m/s, ...
+
+if record.messageNumber != 20
+  error("expect messageNumber==20 (record)");
+end
+
 switch name
   case "lat"
     fieldNum = 0;
@@ -7,12 +16,24 @@ switch name
   case "lon"
     fieldNum = 1;
     scale = 2^32*360; % to deg
+  case "alt"
+    fieldNum = 2;
+    scale = 1;        % not known so far
+  case "heart"
+    fieldNum = 3;
+    scale = 1;        % not known so far
+  case "cad"
+    fieldNum = 4;
+    scale = 1;        % not known so far
   case "dist"
     fieldNum = 5;
     scale = 100;      % to m
   case "speed"
     fieldNum = 6;
     scale = 1000;     % to m/s
+  % 53 fractional cadence
+  % 87 unknown
+  % 88 unknown
   case "time"
     fieldNum = 253;
     scale = 1;        % seconds from 01.01.1990
@@ -20,7 +41,6 @@ switch name
     error(["field " name " unknown"]);
 end
 
-fieldNum
 for i=1:record.fields
   if record.field(i).fieldNum==fieldNum
     data = record.field(i).data/scale;
@@ -28,4 +48,3 @@ for i=1:record.fields
   end
 end
 
-i
