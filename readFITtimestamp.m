4b1bd22f302f1049c84d291c3744ab212d04b2b3
Roland Exler <roland.exler@a1.net>
2022-08-28 10:44:58 +0200

    corrected syntax error processing compressed timestamp

--- readFITtimestamp.m
+++ readFITtimestamp.m
@@ -2,6 +2,6 @@ function record = readFITtimestamp(id, record)
 
 % TODO
 disp("compressed timestamp");
-record.messageType = bitand(record.header, 96) >> 5;
+record.messageType = bitshift(bitand(record.header, 96), -5);
 record.timeOffset = bitand(record.header, 31);
 
