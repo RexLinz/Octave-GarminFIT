c1135a082b520ba29af069af952de379159eb1d4
Roland Exler <roland.exler@a1.net>
2022-09-02 20:47:34 +0200

    put read FIT into function

--- DecodeFIT.m
+++ DecodeFIT.m
@@ -4,7 +4,7 @@
 
 clear
 
-switch 0
+switch 6
   case 0 % 11.08.2022, 13:39-14:02 / SUP (Surf Markus)
     filename = "C8BD3920";
   case 1 % 12.08.2022, 10:32-12:07 / SUP (Surf Markus)
@@ -23,66 +23,7 @@ switch 0
     error("no data selected");
 end
 
-id = fopen(["data/" filename ".fit"],"rb");
-if id<0 error("could not open input file"); end
-fileInfo = stat(id);
-fileSize = fileInfo.size;
-FITheader = readFITheader(id);
-message = {};
-
-%while !feof(id)
-%for r=1:35
-while ftell(id)<fileSize-2 % read to CRC
-  clear record;
-  record.header = fread(id, 1);
-  if feof(id) break; end;
-  if bitand(record.header, 128)
-    disp("compressed timestamp");
-    record = readFITtimestamp(id, record); % TODO (untested)
-  elseif bitand(record.header, 64)
-    msgID = bitand(record.header, 15);
-    record = readFITdefinition(id, record);
-    % now save as new message definition
-    if !feof(id) % eof will occour while reading CRC instead
-      if (length(message)>=msgID+1) && (message{msgID+1}.header!=0)
-        % redefinition of already used message, save backup at end of array
-        message{length(message)+1} = message{msgID+1};
-%        disp(["redefinition of " num2str(msgID)]);
-      else
-%        disp(["definition " num2str(msgID)]);
-      end
-      message{msgID+1} = record;
-    end
-  else
-    msgID = bitand(record.header, 15);
-    record = message{msgID+1};
-%    disp(["data " num2str(msgID) ", messageNumber=" num2str(record.messageNumber) ", fields=" num2str(record.fields)]);
-    nData = length(record.field(1).data);
-    for f = 1:record.fields
-      field = record.field(f);
-      [temp, bytes] = readDataField(id, field);
-      if feof(id)
-        break
-      else
-        if length(temp)==1 % we expect just one value read
-          message{msgID+1}.field(f).data(nData+1) = temp;
-        else
-          message{msgID+1}.field(f).data(nData+1) = temp(1);
-%          warning("multiple data read");
-        end
-      end
-    end
-    % skip developer content if available
-    if record.devFields>0
-      for f=1:record.devFields
-        [~,recSize] = fread(id, record.devField(f).fieldSize);
-      end
-    end
-  end
-end
-
-fclose(id);
-message = addAllNames(message); % add record and field names
+message = readFIT(["data/" filename]);
 
 % optional, useful for analyzing a file, but takes much time
 % add text message and field names
